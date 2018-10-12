8f8179777ae5a262179ff7a8e82f7dd616964312
Lorenz Muri <muri@Lorenz-MBP.local>
2018-10-12 11:48:07 +0200

    ue02_typescript 12-10-2018

--- ue02_typescript/src/main.ts
+++ ue02_typescript/src/main.ts
@@ -1,4 +1,4 @@
 import { Person } from './person';
 
-let p = new Person('Lorenz', 'Muri', 2000);
+const p = new Person('Lorenz', 'Muri', 2000);
 console.log(p.vorname, p.nachname, p.geburtsjahr);
--- ue02_typescript/src/person.ts
+++ ue02_typescript/src/person.ts
@@ -1,23 +1,23 @@
 export class Person {
     private _vorname: string;
-    private _nachmane: string;
+    private _nachname: string;
     private _geburtsjahr: number;
 
     // tslint:disable typedef-whitespace
     public constructor(vorname: string, nachname : string, geburtsjahr: number) {
         this._vorname = vorname;
-        this._nachmane = nachname;
+        this._nachname = nachname;
         this._geburtsjahr = geburtsjahr;
     // tslint:enable typedef-whitespace
     }
 
     public get vorname(): string {
-        return this.vorname;
+        return this._vorname;
     }
     public get nachname(): string {
-        return this.nachname;
+        return this._nachname;
     }
     public get geburtsjahr(): number {
-        return this.geburtsjahr;
+        return this._geburtsjahr;
     }
 }
