f20c7aff9e1e3cbcb32a041d683a78dc6e636181
Lorenz Muri <muri@Lorenz-MBP.local>
2019-01-25 11:48:39 +0100

    FIV-25.01.2019

---
+++ ue04MyWebsite/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TreesComponent } from './trees.component';
+import { CarsComponent } from './cars.component';
+import { ClockComponent } from './clock.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, TreesComponent, CarsComponent,
+    ClockComponent, NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
